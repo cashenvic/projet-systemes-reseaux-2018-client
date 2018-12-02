791812f96f85c719c344e804259f636ec0dfb639
cashenvic <comptes.kh@protonmail.com>
2018-12-02 01:51:53 +0100

    squelette du client

--- main.c
+++ main.c
@@ -8,17 +8,90 @@
  * File:   main.c
  * Author: cash
  *
- * Created on 1 décembre 2018, 21:38
+ * Created on 1 décembre 2018, 21:37
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
 
+#define N_PORT 20000 //le meme numero de port que celui utilisé sur le serveur
+#define T_BUFF 256
+
+//_(°_°)_Tous les prototypes devront finir dans des fichiers headers (*.h)
+
+/** @brief Envoi d'une demande de connexion vers le serveur
+ *  @param int socket : socket (descripteur de fichier) du client qui fait la requete
+ *  @param char *buffer : buffer (tableau de caractères) qui contient la requete envoyée.
+ *  @return void : Pas de valeur de retour
+ **/
+void sendToServer(int socket, char *buffer);
+
+/** @brief Reception des données reçues du serveur
+ *  @param int socket : socket (descripteur de fichier) du client qui fait la requete
+ *  @param char *buffer : buffer (tableau de caractères) qui contient les données reçues.
+ *  @param int c: Etat de la lecture du buffer
+ *  @return char * : Retourne la valeur reçue et lue sous forme de tableau de caractères
+ **/
+char * receiveFromServer(int socket, char *buffer, int n);
 /*
  * 
  */
 int main(int argc, char** argv) {
+    struct sockaddr_in sin = {0}; // adresse du serveur
+    struct hostent *infos_server = NULL;
+    int socket_client;
+    const char *hostname = "localhost"; // nom du serveur
+    char buffer[T_BUFF];
+    int n = 0; // temoin pour la lecture avec le buffer
 
+    socket_client = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_client == -1) {
+        perror("socket_client");
+        exit(-1);
+    }
+
+    infos_server = gethostbyname(hostname); /* on récupère les informations du serveur */
+
+    if (infos_server == NULL) {
+        perror("infos_server");
+        exit(-1);
+    }
+
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = infos_server.h_addr; /* l'adresse se trouve dans le champ h_addr de la structure infos_server */
+    sin.sin_port = htons(N_PORT);
+
+    if (connect(socket_client, (struct sockaddr *) & sin, sizeof (sin)) == -1) {
+        perror("connect");
+        exit(-1);
+    }
+
+    sendToServer(socket_client, buffer);
+    
+    buffer[n] = '\0';
+    printf("Reception de %s", buffer);
+    close(socket_client);
     return (EXIT_SUCCESS);
 }
 
+void sendToServer(int socket, char *buffer) {
+    if (send(socket, buffer, strlen(buffer), 0) == -1) {
+        perror("send");
+        exit(-1);
+    }
+}
+
+char * receiveFromServer(int socket, char *buffer, int n) {
+    if ((n = recv(socket, buffer, sizeof buffer - 1, 0)) == -1 0) {
+        perror("recv()");
+        exit(-1);
+        return buffer;
+    }
+}
